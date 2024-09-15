d461b9492d3064b1cbccfb5bb140b77e16c63883
shah.newaj <newajuiucse@gmail.com>
2024-09-15 16:35:12 +0600

    Handicap Add Recruitment Request

---
+++ pageObjects/HiringManagerPage.py
@@ -0,0 +1,104 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+class HiringManager:
+    link_RecruitmentRequest_menu_xpath = "//a[normalize-space()='Recruitment Request']"
+    link_HiringManager_menu_xpath = "//a[normalize-space()='Hiring Manager']"
+    btn_AddNew_link = "Add New"
+    # add_new = "xpath://a[normalize-space()='Add New']" this will also work for "Add New" button
+    # RecruitmentRequest - RecruitmentRequest Form Page Elements
+    drp_project_id = "department"
+    txt_name_of_HM_id = "name_of_department"
+    txt_position_title_id = "position_title"
+    drp_job_des_name = "job_description"
+    txt_number_pos_id = "number_of_position"
+    drp_present_employee_name = "employee_at_present"
+    txt_location_id = "location"
+    drp_appointment_type_id = "appointment_type"
+    txt_job_starting_date_id = "job_starting_date"
+    txt_job_ending_date_id = "job_ending_date"
+    txt_vacancy_caused_id = "vacancy_caused_due_to"
+    txt_salary_range_from_id = "salary_range_from"
+    drp_recruitment_type_name = "recruitment_type"
+    drp_hod_name = "hodam_id"
+    txt_salary_range_to_id = "salary_range_to"
+    txt_comment_id = "comments"
+    btn_save_draft_name = "save"
+    btn_confirm_submit_name = "save_and_update"
+    action_btn_edit_xpath = "//tr[@id='33']//i[@class='fa fa-edit']"  # Must Change RecruitmentRequest ID in every run
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clickRecruitmentRequest(self):
+        self.driver.find_element(By.XPATH, self.link_RecruitmentRequest_menu_xpath).click()
+
+
+    def clickHiringManager(self):
+        self.driver.find_element(By.XPATH, self.link_HiringManager_menu_xpath).click()
+
+    def clickAddNew(self):
+        self.driver.find_element(By.LINK_TEXT, self.btn_AddNew_link).click()
+
+    def selectProject(self, project):
+        project_value = self.driver.find_element(By.ID, self.drp_project_id)
+        project_name = Select(project_value)
+        project_name.select_by_visible_text(project)
+    def setHiringManager(self, hmanager):
+        self.driver.find_element(By.ID, self.txt_name_of_HM_id).send_keys(hmanager)
+
+    def setVacantPositiion(self, vposition):
+        self.driver.find_element(By.ID, self.txt_vacancy_caused_id).send_keys(vposition)
+
+    def selectJobDescription(self, jobdes):
+        jobdescription_value = self.driver.find_element(By.ID, self.drp_job_des_name)
+        jobdescritoin_name = Select(jobdescription_value)
+        jobdescritoin_name.select_by_visible_text(jobdes)
+
+    def setNumberofPosition(self, npos):
+        self.driver.find_element(By.ID, self.txt_number_pos_id).send_keys(npos)
+
+    def selectPresentEmployee(self, pemployee):
+        presentemployee_value = self.driver.find_element(By.ID, self.drp_present_employee_name)
+        presentemployee_name = Select(presentemployee_value)
+        presentemployee_name.select_by_visible_text(pemployee)
+
+    def setLocation(self, location):
+        self.driver.find_element(By.ID, self.txt_location_id).send_keys(location)
+
+    def selectAppointment(self, appoinment):
+        appoinment_value = self.driver.find_element(By.ID, self.drp_appointment_type_id)
+        appoinment_name = Select(appoinment_value)
+        appoinment_name.select_by_visible_text(appoinment)
+
+    def setJobStartDate(self, jobstartdate):
+        self.driver.find_element(By.ID, self.txt_job_starting_date_id).send_keys(jobstartdate)
+
+    def setJobEndingDate(self, jobenddate):
+        self.driver.find_element(By.ID, self.txt_job_ending_date_id).send_keys(jobenddate)
+
+    def setVacancyCaused(self, vacancycaused):
+        self.driver.find_element(By.ID, self.txt_vacancy_caused_id).send_keys(vacancycaused)
+
+    def setSalaryRangeFrom(self, salaryrangefrom):
+        self.driver.find_element(By.ID, self.txt_salary_range_from_id).send_keys(salaryrangefrom)
+
+    def selectRecruitmentType(self, recruitmenttype):
+        recruitmenttype_value = self.driver.find_element(By.ID, self.drp_recruitment_type_name)
+        recruitmenttype_name = Select(recruitmenttype_value)
+        recruitmenttype_name.select_by_visible_text(recruitmenttype)
+
+    def selecttHOD(self, hod):
+        hod_value = self.driver.find_element(By.ID, self.drp_hod_name)
+        hod_name = Select(hod_value)
+        hod_name.select_by_value(hod)
+
+    def setSalaryRangeTo(self, salaryrangeto):
+        self.driver.find_element(By.ID, self.txt_salary_range_to_id).send_keys(salaryrangeto)
+
+    def setComments(self, comments):
+        self.driver.find_element(By.ID, self.txt_comment_id).send_keys(comments)
+
+    def clickSubmit(self):
+        self.driver.find_element(By.NAME, self.btn_confirm_submit_name).click()
---
+++ testCases/RecruitmentRequest/test_addRecruitmentRequest.py
@@ -0,0 +1,56 @@
+import pytest
+import time
+
+from selenium.webdriver.common.by import By
+
+from pageObjects.LoginPage import LoginPage
+from pageObjects.HiringManagerPage import HiringManager
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+
+class Test_Add_Recruitment_Request:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUsername()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen()  # Logger
+
+    def test_addRecruitmentRequest(self,setup):
+        self.logger.info("************* Add_Recruitment_Request **********")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.driver.maximize_window()
+
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        self.logger.info("************* Login succesful **********")
+
+        self.logger.info("******* Starting Add_Recruitment_Request Test **********")
+        self.addreq = HiringManager(self.driver)
+        self.addreq.clickRecruitmentRequest()
+        self.addreq.clickHiringManager()
+        self.addreq.clickAddNew()
+
+        self.logger.info("************* Providing recruitment request info **********")
+        self.addreq.selectProject("MEAL")
+        self.addreq.setHiringManager("Abdullah")
+        self.addreq.setVacantPositiion("Sqa Automation Pytest")
+        self.addreq.selectJobDescription("Supply Chain Officer")
+        self.addreq.setNumberofPosition("5")
+        self.addreq.selectPresentEmployee("Yes")
+        self.addreq.setLocation("Dhaka")
+        self.addreq.selectAppointment("Permanent")
+        self.addreq.setJobStartDate("20-08-2024")
+        self.addreq.setJobEndingDate("31-12-2025")
+        self.addreq.setVacancyCaused("Resignation")
+        self.addreq.setSalaryRangeFrom("60000")
+        self.addreq.selectRecruitmentType("External Recruitment")
+        self.addreq.selecttHOD("1")
+        self.addreq.setSalaryRangeTo("80000")
+        self.addreq.setComments("As the potential candidate are available externally, we have decided to try to find the candidate through external recruitment process first.")
+        time.sleep(3)
+        flag = self.driver.find_element(By.NAME, "save_and_update")
+        self.driver.execute_script("arguments[0].scrollIntoView();",flag)
+        time.sleep(2)
+        # self.addreq.clickSubmit()
